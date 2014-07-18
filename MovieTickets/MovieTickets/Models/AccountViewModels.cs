489e4beee93e3db37f6d8ee093d23c50eb12e935
User1 <user1@itechart-group.com>
2014-07-18 11:48:08 +0300

    [Update]: added jquery.

---
+++ MovieTickets/MovieTickets/Models/AccountViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Models
+{
+    public class LogInViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
