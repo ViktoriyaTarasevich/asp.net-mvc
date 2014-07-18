58109cbc897207788b6c8124b5330be81bca0d4b
User1 <user1@itechart-group.com>
2014-07-18 13:43:50 +0300

    [Update]: updated views.

---
+++ MovieTickets/MovieTickets/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MovieTickets.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+	}
+}
--- MovieTickets/MovieTickets/Models/AccountViewModels.cs
+++ MovieTickets/MovieTickets/Models/AccountViewModels.cs
@@ -5,27 +5,34 @@ namespace MovieTickets.Models
     public class LogInViewModel
     {
         [Required]
+        [Display(Name = "Имя")]
         public string Login { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
         [Required]
+        [Display(Name = "Запомнить меня?")]
         public bool RememberMe { get; set; }
     }
 
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Имя")]
         public string Login { get; set; }
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [Compare("Password")]
+        [Display(Name = "Подтвердите пароль")]
         public string ConfirmPassword { get; set; }
     }
 
